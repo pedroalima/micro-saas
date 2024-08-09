27407a943d8f2a240ffb2f74e4ce8bc21e119463
Pedro Lima <tsc.pedrolima@gmail.com>
2024-08-09 19:52:13 -0300

    chore: install prisma

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -2,6 +2,11 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
  
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { 
+  auth, 
+  handlers, 
+  signIn, 
+  signOut 
+  } = NextAuth({
   providers: [GitHub, Google],
 })
