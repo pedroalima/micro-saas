f35eca8d01ec4537f470c1c97f883fcb4b259153
Pedro Lima <tsc.pedrolima@gmail.com>
2024-08-10 09:34:09 -0300

    feat: add email provider for auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
  
 export const { 
   auth, 
@@ -8,5 +7,10 @@ export const {
   signIn, 
   signOut 
   } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 })
