9d45da9a70b046958ef533420c2bacc585663181
Pedro Lima <tsc.pedrolima@gmail.com>
2024-08-16 18:00:53 -0300

    style: add user info component

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,8 +1,12 @@
 import { auth } from "@/services/auth"
+import { UserInfo } from "./_components/user-info"
 
 export default async function Page() {
   const session = await auth()
+
   return (
-    <pre>{JSON.stringify(session?.user, null, 1)}</pre>
+    <main className="h-screen flex flex-col justify-center items-center space-y-4">
+      <UserInfo user={session?.user} />
+    </main>
   )
 }
