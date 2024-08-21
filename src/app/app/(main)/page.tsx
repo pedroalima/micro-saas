6488cbe89d749b98e4094a931f53f8468e816f74
Pedro Lima <tsc.pedrolima@gmail.com>
2024-08-21 16:44:52 -0300

    style: add data table component on app page

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,14 +1,14 @@
 import { DashboardAside, DashboardNav, DashboardPage, DashboardTooltip } from "@/components/dashboard"
 import { auth } from "@/services/auth"
-import { TaskCard } from "./_components/task-card"
-import { UserInfo } from "./_components/user-info"
+import { UserInfo } from "../_components/user-info"
+import { TodoDataTable } from "./_components/todo-data-table"
 
 export default async function Page() {
   const session = await auth()
 
   return (
       <DashboardPage>
-        <header className="sticky top-0 z-30 flex justify-between h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:pl-14 pt-8 pb-4">
+        <header className="sticky top-0 z-30 flex justify-between h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:pl-14 pt-8">
           <h1 className="text-xl font-bold px-6">Todo Dashboard</h1>
           <div className="flex justify-around items-center gap-4">
             <UserInfo user={session?.user} />
@@ -21,7 +21,7 @@ export default async function Page() {
             </DashboardNav>
           </DashboardAside>
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-            <TaskCard />
+            <TodoDataTable />
           </main>
         </div>
       </DashboardPage>
