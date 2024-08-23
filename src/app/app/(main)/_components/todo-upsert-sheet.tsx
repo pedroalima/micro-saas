83014218072c45b8ab3e9e5478b74b3e4ac9cfd1
Pedro Lima <tsc.pedrolima@gmail.com>
2024-08-23 16:08:20 -0300

    style: add todo upsert sheet

---
+++ src/app/app/(main)/_components/todo-upsert-sheet.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { useRef } from "react"
+import { useForm } from "react-hook-form"
+import { Todo } from "./todo-data-table"
+
+type TodoUpsertSheetProps = {
+    children: React.ReactNode,
+    defaultValues?: Todo
+}   
+
+export function TodoUpsertSheet({ children }: TodoUpsertSheetProps) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  const form = useForm()
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data)
+  })
+    
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div ref={ref}>
+            {children}
+        </div>
+      </SheetTrigger>
+      <SheetContent>
+            <Form {...form}>
+                <form onSubmit={onSubmit} className="space-y-8">
+                    <SheetHeader>
+                        <SheetTitle>Create todo</SheetTitle>
+                        <SheetDescription>
+                         Create a new todo here.
+                        </SheetDescription>
+                    </SheetHeader>
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Title</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Todo title" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your todo title.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <SheetFooter>
+                        <SheetClose asChild>
+                            <Button type="submit">Create</Button>
+                        </SheetClose>
+                    </SheetFooter>
+                </form>
+            </Form>
+      </SheetContent>
+    </Sheet>
+  )
+}
--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,18 +1,27 @@
 import { DashboardAside, DashboardNav, DashboardPage, DashboardTooltip } from "@/components/dashboard"
+import { Button } from "@/components/ui/button"
 import { auth } from "@/services/auth"
 import { UserInfo } from "../_components/user-info"
 import { TodoDataTable } from "./_components/todo-data-table"
+import { TodoUpsertSheet } from "./_components/todo-upsert-sheet"
 
 export default async function Page() {
   const session = await auth()
 
   return (
       <DashboardPage>
-        <header className="sticky top-0 z-30 flex justify-between h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:pl-14 pt-8">
-          <h1 className="text-xl font-bold px-6">Todo Dashboard</h1>
-          <div className="flex justify-around items-center gap-4">
-            <UserInfo user={session?.user} />
-          </div>
+        <header className="sticky top-0 z-30 h-14 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:pl-14 pt-8">
+          <nav className="flex justify-between items-center">
+            <div className="flex items-center gap-4 px-6">
+              <h1 className="text-xl font-bold">Todo Dashboard</h1>
+              <TodoUpsertSheet>
+                <Button variant="outline">Add Todo</Button>
+              </TodoUpsertSheet>
+            </div>
+            <div className="flex justify-between items-center gap-4">
+              <UserInfo user={session?.user} />
+            </div>
+          </nav>
         </header>
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <DashboardAside>
